25be98f803df1266f14e9f44b47bf28a57220b35
Nicolas <edward.nmontoya@gmail.com>
2021-05-09 19:37:26 -0500

    Creación de documentación y enfoque local.

--- 05-deploy-model.py
+++ 05-deploy-model.py
@@ -6,25 +6,28 @@ from azureml.core.webservice import LocalWebservice
 from azureml.core.conda_dependencies import CondaDependencies
 from azureml.core.webservice import AciWebservice
 
-aciconfig = AciWebservice.deploy_configuration(cpu_cores=0.1, 
+deployment_config = AciWebservice.deploy_configuration(cpu_cores=0.1, 
                                                memory_gb=0.5, 
                                                tags={"data": "Hearth",  "method" : "sklearn"}, 
                                                description='Predict hearth attack with sklearn')
+
+# Despliegue local mediante docker
+#deployment_config = LocalWebservice.deploy_configuration(port=6789)
 
 ws = Workspace.from_config()
 model = Model(ws, 'heart_attack_model')
                    
 print(model.name, model.id, model.version, sep='\t')
 
-# Create an environment and add conda dependencies to it
+# Creciación de azure constainer con ambiente gestionado mediante conda.
 myenv = Environment(name="myenv")
-# Enable Docker based environment
+# Se habilita docker
 myenv.docker.enabled = True
-# Build conda dependencies
+# Definición de dependencias de docker.
 myenv.python.conda_dependencies = CondaDependencies.create(conda_packages=['scikit-learn'],
                                                            pip_packages=['azureml-defaults', 'numpy', 'pandas'])
                                                           
 inf_config = InferenceConfig(environment=myenv, source_directory='./src', entry_script='entry.py')
-service = Model.deploy(ws, "heart-webservice", [model], inf_config, aciconfig)
+service = Model.deploy(ws, "heart-webservice", [model], inf_config, deployment_config)
 service.wait_for_deployment(show_output=True)
 print(service.get_logs())
